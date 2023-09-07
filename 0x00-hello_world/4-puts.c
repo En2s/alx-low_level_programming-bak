e6c4c41df9be549bde18dc14e92238d3f573be79
En2s <enasbdraldeen65@gmail.com>
2023-09-07 13:29:49 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,17 +3,13 @@
  /** 
   * main - Entry point 
   * 
-  * Description: prints "Programming is like building a multilingual puzzle" 
+  * Description: prints "with proper grammar, but the outcome is a piece of 
+  * art," 
   * 
   * Return: Always 0 (Success) 
   */ 
  int main(void) 
  { 
-         puts("\"Programming is like building a multilingual puzzle"); 
+         printf("with proper grammar, but the outcome is a piece of art,\n"); 
          return (0); 
  }
-chmod u+x 4-puts.c
-cd ..
-git add .
-git commit -m 'My first commit'
-git push
